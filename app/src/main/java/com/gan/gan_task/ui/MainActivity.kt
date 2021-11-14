9659d68a57ebe7d98e34c282bf88c8e9e63d1c38
Aniket Indulkar <ai19aan@herts.ac.uk>
2021-11-14 18:40:39 +0000

    Main Activity
    Main Activity moved to UI package.

--- app/src/main/java/com/gan/gan_task/ui/MainActivity.kt
+++ app/src/main/java/com/gan/gan_task/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.gan.gan_task
+package com.gan.gan_task.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gan.gan_task.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
