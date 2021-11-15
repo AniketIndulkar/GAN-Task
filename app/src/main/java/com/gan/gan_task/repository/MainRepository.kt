4ca958e20ea73c0ecbafa3f6caf59142b07d6b53
Aniket Indulkar <ai19aan@herts.ac.uk>
2021-11-15 10:47:17 +0000

    Update MainRepository
    Updated main repository with some additional functionality.

--- app/src/main/java/com/gan/gan_task/repository/MainRepository.kt
+++ app/src/main/java/com/gan/gan_task/repository/MainRepository.kt
@@ -1,5 +1,6 @@
 package com.gan.gan_task.repository
 
+import android.util.Log
 import com.gan.gan_task.model.BBCharacter
 import com.gan.gan_task.retrofit.BBCharacterAPI
 import com.gan.gan_task.retrofit.NetworkMapper
@@ -20,7 +21,7 @@ class MainRepository constructor(
         emit(DataState.Loading)
         try{
             val bbCharactersFromNetwork = bbCharacterAPI.getAllCharacters()
-            val bbCharacters = networkMapper.mapFromNetworkEntityList(bbCharactersFromNetwork)
+            val bbCharacters = networkMapper.mapFromNetworkEntityList(bbCharactersFromNetwork).sortedBy { it.name }
             for(character in bbCharacters){
                 bbCharacterDao.insertBBCharacter(roomMapper.mapToEntity(character))
             }
@@ -29,5 +30,41 @@ class MainRepository constructor(
         }catch (e: Exception){
             emit(DataState.Error(e))
         }
+    }
+
+
+    suspend fun searchCharacter(searchString : String): Flow<DataState<List<BBCharacter>>> = flow {
+        emit(DataState.Loading)
+        try{
+
+            val savedBBCharacter = bbCharacterDao.searchCharacter(searchString)
+            emit(DataState.Success(roomMapper.mapFromRoomEntityList(savedBBCharacter)))
+        }catch (e: Exception){
+            emit(DataState.Error(e))
+        }
+    }
+
+    suspend fun filterCharacter(): Flow<DataState<List<BBCharacter>>> = flow {
+        emit(DataState.Loading)
+        try{
+
+            val savedBBCharacter = bbCharacterDao.getAllCharacters().sortedBy { it.char_id }
+            emit(DataState.Success(roomMapper.mapFromRoomEntityList(savedBBCharacter)))
+        }catch (e: Exception){
+            emit(DataState.Error(e))
+        }
+    }
+
+    suspend fun updateCharacterData(charData : BBCharacter){
+        var affectedData= bbCharacterDao.getCharacterById(31)
+        Log.d("MainRepository", "Before: ${affectedData.isFav}")
+        val rowAffected= bbCharacterDao.insertBBCharacter(roomMapper.mapToEntity(charData))
+        affectedData= bbCharacterDao.getCharacterById(31)
+        Log.d("MainRepository", "After: ${affectedData.isFav}")
+    }
+
+    suspend fun getCharacterById(charId: Int): BBCharacter{
+        val character =bbCharacterDao.getCharacterById(charId)
+        return roomMapper.mapFromEntity(character)
     }
 }
