e7f43502fd1cbef01dc088f3e67c83f48c20ad4f
Aniket Indulkar <ai19aan@herts.ac.uk>
2021-11-15 10:51:16 +0000

    Update Characters DAO
    Search and get user by id methods added to implement search and Detail activity.

--- app/src/main/java/com/gan/gan_task/room/BBCharacterDAO.kt
+++ app/src/main/java/com/gan/gan_task/room/BBCharacterDAO.kt
@@ -5,6 +5,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.gan.gan_task.model.BBCharacter
 
 @Dao
 interface BBCharacterDAO {
@@ -12,7 +13,13 @@ interface BBCharacterDAO {
     @Query("SELECT * FROM BBCharacter ORDER BY NAME")
     suspend fun getAllCharacters() : List<BBCharacterRoomEntity>
 
+    @Query("SELECT * FROM BBCharacter WHERE char_id =:charId")
+    suspend fun getCharacterById(charId : Int) : BBCharacterRoomEntity
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertBBCharacter(bbCharacter : BBCharacterRoomEntity): Long
+
+    @Query("SELECT * FROM BBCharacter WHERE name LIKE '%' || :search || '%'")
+    suspend fun searchCharacter(search: String?): List<BBCharacterRoomEntity>
 
 }
